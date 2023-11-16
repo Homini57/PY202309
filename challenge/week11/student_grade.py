bcd50c8ba9a679212aaeaf9abcbea108392ee27f
Homini <mina3480@naver.com>
2023-11-16 16:15:40 +0900

    week11 머드게임 개선2

--- challenge/week11/student_grade.py
+++ challenge/week11/student_grade.py
@@ -1,8 +1,15 @@
+# 학생 이름 및 성적을 저장하는 클래스
 class Student:
     이름 = ''
     국어 = 0
     수학 = 0
     영어 = 0
+    def __init__(self, 이름, 국어, 수학, 영어):
+        self.이름 = 이름
+        self.국어 = 국어
+        self.수학 = 수학
+        self.영어 = 영어
+
     # 학생 별 성적의 평균 점수를 출력
     def get_average(self):
         sum_num = self.국어 + self.수학 + self.영어
@@ -22,11 +29,7 @@ def loadData (lines):
     subject_list = preprocessed_lines.pop(0)
     
     for line in preprocessed_lines:
-        학생 = Student()
-        학생.이름 = line[0]
-        학생.국어 = float(line[1])
-        학생.수학 = float(line[2])
-        학생.영어 = float(line[3])
+        학생 = Student(line[0], float(line[1]), float(line[2]), float(line[3]))
         student_list.append(학생)
 
     return student_list
